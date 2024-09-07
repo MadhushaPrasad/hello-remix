3a4f228e86134504e2da27b6223a3c13f86d87eb
MadhushaPrasad <madushaprasad21@gmail.com>
2024-09-07 08:53:56 +0530

    Client Side Routing

--- app/root.tsx
+++ app/root.tsx
@@ -3,6 +3,7 @@ import {
   Meta,
   Outlet,
   Scripts,
+  Link,
   ScrollRestoration,
 } from "@remix-run/react";
 import "./tailwind.css";
@@ -36,7 +37,18 @@ export function Layout({ children }: { children: React.ReactNode }) {
 export default function App() {
   return (
     <div>
-      <div id="sidebar">{/* other elements */}</div>
+      <div id="sidebar">
+        <nav>
+          <ul>
+            <li>
+              <Link to={`/contacts/1`}>Your Name</Link>
+            </li>
+            <li>
+              <Link to={`/contacts/2`}>Your Friend</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <div id="detail">
         <Outlet />
       </div>
